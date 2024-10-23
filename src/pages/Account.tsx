f408e49f7706ec3c4fee5dc414011ad8415fc956
eric-antonyan <antonyaneric@gmail.com>
2024-10-23 12:29:31 -0400

    commit

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -43,37 +43,41 @@ const Account = () => {
     };
 
     return (
-        <div className="mx-auto">
-            <header className={`flex items-center justify-between p-4 border-b text-darker border-gray-300 `}>
-                <Link to={"/"}>
-                    <FaChevronLeft className="" />
-                </Link>
-                <span className="font-bold text-lg">{userData.username ? userData.username : `${userData.first_name} ${userData.last_name}`}</span>
-                <FaGear />
-            </header>
-            <div className="flex flex-col items-center mt-6">
-                {userData.photo_url ? (
-                    userData.photo_url !== "letters" ? (
-                        <img
-                            src={userData.photo_url}
-                            alt={`${userData.first_name} ${userData.last_name}`}
-                            className={`w-[150px] aspect-square rounded-full border-2 border-primary`}
-                        />
+        userData && (
+            <div className="mx-auto">
+                <header className={`flex items-center justify-between p-4 border-b text-darker border-gray-300 `}>
+                    <Link to={"/"}>
+                        <FaChevronLeft className=""/>
+                    </Link>
+                    <span
+                        className="font-bold text-lg">{userData.username ? userData.username : `${userData.first_name} ${userData.last_name}`}</span>
+                    <FaGear/>
+                </header>
+                <div className="flex flex-col items-center mt-6">
+                    {userData.photo_url ? (
+                        userData.photo_url !== "letters" ? (
+                            <img
+                                src={userData.photo_url}
+                                alt={`${userData.first_name} ${userData.last_name}`}
+                                className={`w-[150px] aspect-square rounded-full border-2 border-primary`}
+                            />
+                        ) : (
+                            <div
+                                className={`w-[150px] aspect-square rounded-full border-2 border-primary text-primary text-5xl font-bold flex items-center justify-center`}>
+                                {userData.first_name[0].toUpperCase()}
+                            </div>
+                        )
                     ) : (
-                        <div className={`w-[150px] aspect-square rounded-full border-2 border-primary text-primary text-5xl font-bold flex items-center justify-center`}>
-                            {userData.first_name[0].toUpperCase()}
-                        </div>
-                    )
-                ) : (
-                    <p>No photo available</p>
-                )}
-                <h2 className={`mt-4 text-xl font-semibold text-white`}>{userData.first_name} {userData.last_name}</h2>
-                <p className="text-gray-600">{userData.username ? `@${userData.username}` : null}</p>
-                <button className={`bg-primary px-3 py-2 text-sm text-white rounded-2xl font-bold mt-4`}>
-                    {format(userData.balance)}
-                </button>
+                        <p>No photo available</p>
+                    )}
+                    <h2 className={`mt-4 text-xl font-semibold text-white`}>{userData.first_name} {userData.last_name}</h2>
+                    <p className="text-gray-600">{userData.username ? `@${userData.username}` : null}</p>
+                    <button className={`bg-primary px-3 py-2 text-sm text-white rounded-2xl font-bold mt-4`}>
+                        {format(userData.balance)}
+                    </button>
+                </div>
             </div>
-        </div>
+        )
     );
 };
 
